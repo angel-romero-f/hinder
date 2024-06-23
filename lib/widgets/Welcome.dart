3f2f65a8c587719048d5e560bb0ddf77b283e2a9
angel-romero-f <angelfloromero@gmail.com>
2024-06-23 02:47:36 -0500

    Changed stuff

--- lib/widgets/Welcome.dart
+++ lib/widgets/Welcome.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hinder/main.dart';
+import 'package:hinder/widgets/profile_info.dart';
 import 'package:provider/provider.dart';
 
 class TabBarApp extends StatelessWidget {
@@ -9,41 +10,38 @@ class TabBarApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.home),
+        length: 3,
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: const Color.fromARGB(0, 139, 171, 110),
+              bottom: const TabBar(
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.message),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.home),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.person),
+                  )
+                ],
               ),
-              Tab(
-                icon: Icon(Icons.settings),
-              ),
-              Tab(
-                icon: Icon(Icons.person),
-              )
-            ],
-          ),
-          title: const Text('Flutter mapp'),
-        ),
-        body: TabBarView(
-          children: [
-            Container(
-              color: Colors.orange,
-              child: const Icon(Icons.home),
             ),
-            Container(
-              color: Colors.orange,
-              child: const Icon(Icons.home),
-            ),
-            Container(
-              color: Colors.orange,
-              child: const Icon(Icons.home),
-            )
-          ],
-          )
-      )
-    );
+            body: TabBarView(
+              children: [
+                Container(
+                  color: Colors.orange,
+                  child: const Icon(Icons.home),
+                ),
+                Container(
+                  color: Colors.orange,
+                  child: const Icon(Icons.home),
+                ),
+                ProfileInfo()
+              ],
+            )));
   }
 }
+
+
