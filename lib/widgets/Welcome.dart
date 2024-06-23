34d18f12e0b1d5356a1ba2be9fe6bab88795a16d
Edgar Brizuela <edgarbrizuela@Edgars-MacBook-Pro.local>
2024-06-23 02:30:39 -0500

    simplified code with tab bar and navigation

--- lib/widgets/Welcome.dart
+++ lib/widgets/Welcome.dart
@@ -1,77 +1,49 @@
 import 'package:flutter/material.dart';
-
-void main() => runApp(const TabBarApp());
+import 'package:flutter/widgets.dart';
+import 'package:hinder/main.dart';
+import 'package:provider/provider.dart';
 
 class TabBarApp extends StatelessWidget {
-  const TabBarApp({super.key});
+  const TabBarApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const TabBarExample(),
-    );
-  }
-}
-
-class TabBarExample extends StatefulWidget {
-  const TabBarExample({super.key});
-
-  @override
-  State<TabBarExample> createState() => _TabBarExampleState();
-}
-
-class _TabBarExampleState extends State<TabBarExample>
-    with TickerProviderStateMixin {
-  late final TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    _tabController = TabController(length: 3, vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('TabBar Sample'),
-        bottom: TabBar(
-          controller: _tabController,
-          tabs: const <Widget>[
-            Tab(
-              icon: Icon(Icons.cloud_outlined),
-            ),
-            Tab(
-              icon: Icon(Icons.beach_access_sharp),
-            ),
-            Tab(
-              icon: Icon(Icons.brightness_5_sharp),
-            ),
-          ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                icon: Icon(Icons.settings),
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+              )
+            ],
+          ),
+          title: const Text('Flutter mapp'),
         ),
-      ),
-      body: TabBarView(
-        controller: _tabController,
-        children: const <Widget>[
-          Center(
-            child: Text("It's cloudy here"),
-          ),
-          Center(
-            child: Text("It's rainy here"),
-          ),
-          Center(
-            child: Text("It's sunny here"),
-          ),
-        ],
-      ),
+        body: TabBarView(
+          children: [
+            Container(
+              color: Colors.orange,
+              child: const Icon(Icons.home),
+            ),
+            Container(
+              color: Colors.orange,
+              child: const Icon(Icons.home),
+            ),
+            Container(
+              color: Colors.orange,
+              child: const Icon(Icons.home),
+            )
+          ],
+          )
+      )
     );
   }
 }
-
